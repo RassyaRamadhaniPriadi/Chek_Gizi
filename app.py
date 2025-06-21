299433adfa4bd9a4151cc0ed29f4df61035509a1
RassyaRamadhaniPriadi <160223450+RassyaRamadhaniPriadi@users.noreply.github.com>
2025-06-21 21:27:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ if not GEMINI_API_KEY:
 
 # --- Konfigurasi Gemini ---
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel('gemini-2.0-flash')
+model = genai.GenerativeModel('gemini-1.5-flash')
 
 # --- Path folder ---
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
